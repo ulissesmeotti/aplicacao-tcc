57af3a2e919da6afc7816153a5e8b41672f7c9e4
ulissesmeotti <ulissesmeotti@gmail.com>
2025-06-02 14:37:34 -0300

    ..

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
-  // optimizeDeps: {
-  //   exclude: ['lucide-react'],
-  // }
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  }
 });
